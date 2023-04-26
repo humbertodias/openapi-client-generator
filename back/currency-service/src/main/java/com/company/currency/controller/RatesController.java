3b31f2ef39e1114d66bd0523c3a7da96241d83cc
Humberto Dias <hldtux@gmail.com>
2023-04-25 23:01:18 -0300

    cors

--- back/currency-service/src/main/java/com/company/currency/controller/RatesController.java
+++ back/currency-service/src/main/java/com/company/currency/controller/RatesController.java
@@ -3,10 +3,7 @@ package com.company.currency.controller;
 import com.company.currency.domain.cdn.CurrenciesDate;
 import com.company.currency.service.CdnGettingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
