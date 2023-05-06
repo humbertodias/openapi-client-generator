0927aa473776dbd3ede3c4fb080adb762fd79c8e
Humberto <hldtux@gmail.com>
2023-05-05 23:12:56 -0300

    unused

--- back/rates-plugin-api/main/java/com/company/rates/swagger/api/RatesControllerApiClient.java
+++
@@ -1,8 +0,0 @@
-package com.company.rates.swagger.api;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import io.swagger.configuration.ClientConfiguration;
-
-@FeignClient(contextId="RatesControllerApiClient", name="${openAPIDefinition.name:openAPIDefinition}", url="${openAPIDefinition.url:http://localhost:8006}", configuration = ClientConfiguration.class)
-public interface RatesControllerApiClient extends RatesControllerApi {
-}
