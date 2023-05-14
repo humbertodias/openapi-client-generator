3fcf22023e9a04138cbe657d2a00f1b7ce88eb5c
Humberto Dias <hldtux@gmail.com>
2023-05-14 00:32:07 -0300

    dockerfile build

--- back/rates-plugin-api/build-push.sh
+++ back/rates-plugin-api/build-push.sh
@@ -4,6 +4,6 @@ IMAGE=maven-rates-plugin-api
 docker rmi $IMAGE
 docker rmi hldtux/$IMAGE
 # build and push
-docker build . -t $IMAGE && \
+docker build --platform linux/amd64,linux/arm64 . -t $IMAGE --no-cache && \
 docker tag $IMAGE hldtux/$IMAGE && \
 docker push hldtux/$IMAGE && echo "Pushed"
