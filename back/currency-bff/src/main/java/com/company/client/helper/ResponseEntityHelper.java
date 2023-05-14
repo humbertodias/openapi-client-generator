7acbe975177abe1080e0f3acb35e7aa017c840f5
Humberto Dias <hldtux@gmail.com>
2023-05-14 11:10:58 -0300

    optimization

--- back/currency-bff/src/main/java/com/company/client/helper/ResponseEntityHelper.java
+++ back/currency-bff/src/main/java/com/company/client/helper/ResponseEntityHelper.java
@@ -2,12 +2,15 @@ package com.company.client.helper;
 
 import org.springframework.http.ResponseEntity;
 
-public class ResponseEntityHelper {
+public final class ResponseEntityHelper {
     private ResponseEntityHelper(){
 
     }
 
     public static ResponseEntity<?> forward(ResponseEntity<?> responseEntity){
+        if (responseEntity == null) {
+            throw new IllegalArgumentException("responseEntity cannot be null");
+        }
         return ResponseEntity.status(responseEntity.getStatusCode()).body(responseEntity.getBody());
     }
 }
