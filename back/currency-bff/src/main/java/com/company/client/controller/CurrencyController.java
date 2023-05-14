e85e142290c3e2698e1577434b5d7549782e847a
Humberto Dias <hldtux@gmail.com>
2023-05-14 01:15:19 -0300

    cors

--- back/currency-bff/src/main/java/com/company/client/controller/CurrencyController.java
+++ back/currency-bff/src/main/java/com/company/client/controller/CurrencyController.java
@@ -4,13 +4,9 @@ import com.company.rates.swagger.api.RatesControllerApiClient;
 import com.company.rates.swagger.model.CurrenciesDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+import java.util.List;@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "${bff.rest.api.prefix}")
